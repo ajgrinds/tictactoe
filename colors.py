b3eb51f988d57261934f4ad2f3ad78ab5cb99387
ajgrinds <ajgrinds03@gmail.com>
2022-02-01 12:24:52 -0800

    Added color changing and more

---
+++ colors.py
@@ -0,0 +1,15 @@
+light_colors = {
+    "background_color": "#fbf1f2",
+    "x_color": "#1d2021",
+    "o_color": "#504945",
+    "sidebar_color": "#e6e3dc",
+    "lines_color": "black",
+}
+
+dark_colors = {
+    "background_color": "#282828",
+    "x_color": "#af3a03",
+    "o_color": "#ebdbb2",
+    "sidebar_color": "#1d2021",
+    "lines_color": "#d65d0e",
+}
--- graphics.py
+++ graphics.py
@@ -1,9 +1,10 @@
 import pygame
 from tictactoe import TicTacToe
+from colors import dark_colors, light_colors
 
 pygame.init()
-SCREEN_HEIGHT = 1080
-SCREEN_WIDTH = 1920
+SCREEN_HEIGHT = 400
+SCREEN_WIDTH = 500
 SQUARE_SIZE = min(SCREEN_WIDTH, SCREEN_HEIGHT)
 
 
@@ -33,6 +34,17 @@ def main():
     dark_mode_img = pygame.transform.scale(pygame.image.load("darkmode.png"), (SQUARE_SIZE // 10, SQUARE_SIZE // 10))
     dark_mode_button = Button((SQUARE_SIZE * 1.05, 0), dark_mode_img)
 
+    lines = [
+        pygame.Rect(SQUARE_SIZE // 3 - SQUARE_SIZE // 100, SQUARE_SIZE // 50, SQUARE_SIZE // 50,
+                    SQUARE_SIZE - SQUARE_SIZE // 50),
+        pygame.Rect(2 * SQUARE_SIZE // 3 - SQUARE_SIZE // 100, SQUARE_SIZE // 50, SQUARE_SIZE // 50,
+                    SQUARE_SIZE - SQUARE_SIZE // 50),
+        pygame.Rect(SQUARE_SIZE // 50, SQUARE_SIZE // 3 - SQUARE_SIZE // 100, SQUARE_SIZE - SQUARE_SIZE // 50,
+                    SQUARE_SIZE // 50),
+        pygame.Rect(SQUARE_SIZE // 50, 2 * SQUARE_SIZE // 3 - SQUARE_SIZE // 100, SQUARE_SIZE - SQUARE_SIZE // 50,
+                    SQUARE_SIZE // 50)
+    ]
+
     game = TicTacToe(players=2)
 
     shutdown = False
@@ -40,11 +52,9 @@ def main():
         if dark_mode_button.clicked():
             dark_mode = not dark_mode
         if dark_mode:
-            background_color = pygame.Color("black")
-            text_color = pygame.Color("blue")
+            colors = dark_colors
         else:
-            background_color = pygame.Color("white")
-            text_color = pygame.Color("black")
+            colors = light_colors
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 shutdown = True
@@ -68,17 +78,13 @@ def main():
                 if column and row:
                     game.make_move((column, row))
 
-        screen.fill(background_color)
-        pygame.draw.rect(screen, text_color, pygame.Rect(SQUARE_SIZE // 3 - SQUARE_SIZE // 100, 0, SQUARE_SIZE // 50, SQUARE_SIZE))
-        pygame.draw.rect(screen, text_color, pygame.Rect(2 * SQUARE_SIZE // 3 - SQUARE_SIZE // 100, 0, SQUARE_SIZE // 50, SQUARE_SIZE))
-        pygame.draw.rect(screen, text_color, pygame.Rect(0, SQUARE_SIZE // 3 - SQUARE_SIZE // 100, SQUARE_SIZE, SQUARE_SIZE // 50))
-        pygame.draw.rect(screen, text_color, pygame.Rect(0, 2 * SQUARE_SIZE // 3 - SQUARE_SIZE // 100, SQUARE_SIZE, SQUARE_SIZE // 50))
-        pygame.draw.rect(screen, text_color, pygame.Rect(0, SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE // 50))
-        pygame.draw.rect(screen, text_color, pygame.Rect(SQUARE_SIZE, 0, SQUARE_SIZE // 50, SQUARE_SIZE))
+        screen.fill(colors["sidebar_color"])
+        pygame.draw.rect(screen, colors["background_color"], pygame.Rect(0, 0, SQUARE_SIZE, SQUARE_SIZE))
+
         screen.blit(dark_mode_button.image, dark_mode_button.rect)
 
-        x_label = font.render(TicTacToe.get_symbol(1), True, text_color)
-        o_label = font.render(TicTacToe.get_symbol(2), True, text_color)
+        x_label = font.render(TicTacToe.get_symbol(1), True, colors["x_color"])
+        o_label = font.render(TicTacToe.get_symbol(2), True, colors["o_color"])
         for i, row in enumerate(game.board):
             for j, spot in enumerate(row):
                 if spot == TicTacToe.get_symbol(1):
@@ -86,12 +92,15 @@ def main():
                 if spot == TicTacToe.get_symbol(2):
                     screen.blit(o_label, (i * (SQUARE_SIZE // 3), j * (SQUARE_SIZE // 3)))
 
+        for line in lines:
+            pygame.draw.rect(screen, colors["lines_color"], line)
+
         if winner := game.win():
-            screen.fill(background_color)
-            winner_label = font.render(TicTacToe.get_symbol(winner), True, text_color)
+            screen.fill(colors["background_color"])
+            winner_label = font.render(TicTacToe.get_symbol(winner), True, colors["x_color"])
             winner_label_rect = winner_label.get_rect(center=(SQUARE_SIZE // 2, SQUARE_SIZE // 3))
             screen.blit(winner_label, winner_label_rect)
-            text = lil_font.render("won the game!", True, text_color)
+            text = lil_font.render("won the game!", True, colors["o_color"])
             text_rect = text.get_rect(center=(SQUARE_SIZE // 2, SQUARE_SIZE // 2))
             screen.blit(text, text_rect)
 
