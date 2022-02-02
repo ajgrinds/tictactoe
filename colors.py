f9f30e498a5a1e7fcc75b05f25c92c35fd3e4618
Andrew Grindstaff <ajgrinds03@gmail.com>
2022-02-01 16:17:04 -0800

    Update colors.py

--- colors.py
+++ colors.py
@@ -1,17 +1,19 @@
+import pygame
+
 light_colors = {
-    "background_color": "#fbf1f2",
-    "tile_color": "#cccccc",
-    1: "#1d2021",  # x color
-    2: "#504945",  # o color
-    "sidebar_color": "#e6e3dc",
-    "lines_color": "black",
+    "background_color": pygame.Color("#fbf1f2"),
+    "tile_color": pygame.Color("#cccccc"),
+    1: pygame.Color("#1d2021"),  # x color
+    2: pygame.Color("#504945"),  # o color
+    "sidebar_color": pygame.Color("#e6e3dc"),
+    "lines_color": pygame.Color("black"),
 }
 
 dark_colors = {
-    "background_color": "#282828",
-    "tile_color": "#323232",
-    1: "#af3a03",  # x color
-    2: "#ebdbb2",  # o color
-    "sidebar_color": "#1d2021",
-    "lines_color": "#d65d0e",
+    "background_color": pygame.Color("#282828"),
+    "tile_color": pygame.Color("#323232"),
+    1: pygame.Color("#af3a03"),  # x color
+    2: pygame.Color("#ebdbb2"),  # o color
+    "sidebar_color": pygame.Color("#1d2021"),
+    "lines_color": pygame.Color("#d65d0e"),
 }
